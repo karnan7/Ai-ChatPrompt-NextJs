9737769e17ff2db2137e7d2f070b286b41718b9e
karnan7 <adithkayalad@gmail.com>
2023-06-21 23:25:02 +0530

    mongodb connect

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,18 +1,26 @@
 import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google';
 
+console.log({
+    clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+})
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: '',
-            clientSecret: '',
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
     async session({ session }){
 
     },
     async signIn({ profile }){
+        try {
             
+        } catch (error) {
+            console.log(error);
+        }
     }
 })
 
---
+++ utils/database.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const isConnected = true;
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery', true);
+
+    if (isConnected) {
+        console.log('mongodb is already connected');
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "share_prompt",
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+
+        isConnected = true;
+        console.log('Mongodb connected')
+    } catch (error) {
+        console.log(error);
+    }
+}
