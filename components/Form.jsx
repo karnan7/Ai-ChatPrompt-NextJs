a3d5765a38e06bae708a880a8dcec2575f748aa8
karnan7 <adithkayalad@gmail.com>
2023-07-08 23:40:37 +0530

    post page

--- components/Form.jsx
+++ components/Form.jsx
@@ -6,6 +6,9 @@ const Form = ({ type, setPost, post, submitting, handleSubmit }) => {
       <h1 className="head_text text-left">
         <span className="blue_gradient">{type} post</span>
       </h1>
+      <p className="desc text-left max-w-md">
+        {type} and share amazing prompts with the world and let your imagination run wild with any AI-Powered platform
+      </p>
     </section>
   )
 }
