84c59f2dbac520fb04ad83471c7b6b37fab4a118
karnan7 <adithkayalad@gmail.com>
2023-07-11 22:07:41 +0530

    prompt model importing

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,10 +1,18 @@
 import { connectToDB } from "@utils/database";
+import prompt from "@models/prompt";
 
 export const POST = async (req) => {
     const { userID, prompt, tag } = await req.json();
 
     try {
         await connectToDB();
+        const newPrompt = new prompt({
+            creator:userID,
+            prompt,
+            tag,
+        })
+
+        await newPrompt.save();
     } catch (error) {
         
     }
