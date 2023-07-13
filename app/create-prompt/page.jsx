e306a92945d94309890b6aed9d4fb8a5fac84a90
karnan7 <adithkayalad@gmail.com>
2023-07-13 23:04:01 +0530

    error solving

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react'
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 import Form from '@components/Form';
 
