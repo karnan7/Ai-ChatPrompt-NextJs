ec1504199a13e291652c5e30f90f4a1aca2bb05c
karnan7 <adithkayalad@gmail.com>
2023-07-26 12:07:17 +0530

    deployment bugs

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -7,7 +7,7 @@ import { connectToDB } from "@utils/database";
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
